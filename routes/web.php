69dc7a569dcc20defb2319bacb8691c1c401c1b0
RockboundNeighbors <sodapoppinff@gmail.com>
2019-10-04 22:06:25 +0900

    ルーティング追記
    骨組み

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('Examination')->group(function(){
+	//記事関連
+	Route::get('titleaddform','ExamController@titleaddform');
+	Route::post('titleadd','ExamController@titleadd');
+	Route::get('titlelists','ExamController@titlelists');
+	Route::post('titleedit','ExamController@titleedit');
+	Route::post('titledelete','ExamController@titledelete');
+	
+	//カテゴリー関連
+	Route::get('categorieslist','ExamController@categorieslist');
+	Route::get('categoryaddform','ExamController@categoryaddform');
+	Route::post('categoryedit','ExamController@categoryedit');
+	Route::post('categorydelete','ExamController@categorydelete');
+
+	//閲覧関連
+	Route::get('top',function(){
+		return view('Examination/top');
+	});
+});
